75467c9bffad2d3c056567830b67a73463e67642
Angel Romero <mrangelrcd@gmail.com>
2023-07-31 12:14:02 +0200

    Create game page basic visual style

---
+++ scripts/drawBoard.js
@@ -0,0 +1,11 @@
+const boardElement = document.querySelector("#board");
+console.log(boardElement);
+for (let row = 0; row < 6; row++){
+  for (let col = 0; col < 7; col++){
+    const newCell = document.createElement("div");
+    newCell.classList.add("cell");
+    newCell.setAttribute("data-col", col);
+    newCell.setAttribute("data-row", row);
+    boardElement.appendChild(newCell);
+  }
+}
